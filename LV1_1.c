2a37703bdfedb09d38b69dd4e21cb2a2d7772d69
Domagoj Šutalo <dsutalo@etfos.hr>
2020-12-02 19:15:00 +0100

    Cherry pick odrađen prije promjene teksta aplikacije

--- LV1_1.c
+++ LV1_1.c
@@ -2,6 +2,10 @@
 
 int main(){
     
+<<<<<<< HEAD
     printf("Pozdrav iz aplikacije od Domagoja Šutala i šaljem broj indeksa: 6501(izmišljen broj)");
+=======
+    printf("Pozdrav iz aplikacije od Domagoja Šutala");
+>>>>>>> 6acc9f5... Dodana aplikacija s pozdravom
     return 0;
 }
